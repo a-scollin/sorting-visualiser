8647d53118f8b174bfa384b1b42720f00c5039b8
Andrew <andrewscollin@hotmail.co.uk>
2020-08-24 15:52:14 +0100

    Updates

---
+++ precache-manifest.eded82f04e12b1fa0ad1146bfcbd90fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a9e8da96f16c11ff48b787b5c296e52",
+    "url": "/Sorting/index.html"
+  },
+  {
+    "revision": "75f6be02af99cfeb3d76",
+    "url": "/Sorting/static/css/main.5ff61248.chunk.css"
+  },
+  {
+    "revision": "05a84a294611bfae3e92",
+    "url": "/Sorting/static/js/2.5c17aa9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sorting/static/js/2.5c17aa9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75f6be02af99cfeb3d76",
+    "url": "/Sorting/static/js/main.4889360a.chunk.js"
+  },
+  {
+    "revision": "192a08f6dcb7470891f0",
+    "url": "/Sorting/static/js/runtime-main.68512179.js"
+  }
+]);
